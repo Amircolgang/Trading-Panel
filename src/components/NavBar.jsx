ab2fa3fad16e995458c6ed9706a61913bc2bb523
amircolgang <parham38385747@gmail.com>
2025-08-06 17:47:25 +0330

    Amircolgang: changes on 2025-08-06 17:47:24

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 export default function NavBar() {
   return (
     <>
-        <div className='h-[65px] flex justify-between items-center bg-[#111]'>
+        <div className='h-[60px] p-[1.5rem] flex justify-between items-center bg-[#111]'>
             <div>
                 <div className='w-[25px] h-[25px]'>
                     <img className='w-[100%] h-[100%]' src="/public/vite.svg" alt="" />
                 </div>
-                <span></span>
+                <span>Shirazi Co.</span>
             </div>
             <div>
                 <ul className='flex gap-[1.5rem] text-[#fff]'>                    <li>Home </li>
