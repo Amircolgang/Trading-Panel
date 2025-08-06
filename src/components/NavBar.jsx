5fc06309029bd79d795f691aaca751c81bc1b816
amircolgang <parham38385747@gmail.com>
2025-08-06 15:52:16 +0330

    Amircolgang: changes on 2025-08-06 15:52:16

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -20,7 +20,7 @@ export default function NavBar() {
             </div>
             <div>
                 <div>
-
+                    
                 </div>
                 <div>
                     <img src="" alt="" />
