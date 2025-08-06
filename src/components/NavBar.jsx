06f0fee6d4f48f96e5a6face9fd1f7d564b64b4a
amircolgang <parham38385747@gmail.com>
2025-08-06 17:48:30 +0330

    Amircolgang: changes on 2025-08-06 17:48:29

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,11 +4,11 @@ export default function NavBar() {
   return (
     <>
         <div className='h-[60px] p-[1.5rem] flex justify-between items-center bg-[#111]'>
-            <div>
+            <div className='flex items-center gap-[0.5rem]'>
                 <div className='w-[25px] h-[25px]'>
                     <img className='w-[100%] h-[100%]' src="/public/vite.svg" alt="" />
                 </div>
-                <span>Shirazi Co.</span>
+                <span className='text-[#fff]'>Shirazi Co.</span>
             </div>
             <div>
                 <ul className='flex gap-[1.5rem] text-[#fff]'>                    <li>Home </li>
